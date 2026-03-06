01e1a4de6cf79e51dffe468988d4332cd27d230e
Samory Sundjata <samory.sundjata@gmail.com>
2026-03-06 16:06:25 -0300

    Exibição de mensagem pelo  Angular Material

--- frontend/acervo-web/src/app/features/autores/store/autores.effects.ts
+++ frontend/acervo-web/src/app/features/autores/store/autores.effects.ts
@@ -1,42 +1,101 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, mergeMap, of } from 'rxjs';
-import { AutorService } from '../services/autor.service';
-import { AutoresActions } from './autores.actions';
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { catchError, map, mergeMap, tap } from "rxjs/operators";
+import { of } from "rxjs";
+import { AutorService } from "../services/autor.service";
+import { AutoresActions } from "./autores.actions";
+import { NotificationService } from "../../../core/services/notification.service";
 
 @Injectable()
 export class AutoresEffects {
-  loadAutores$ = createEffect(() => this.actions$.pipe(
-    ofType(AutoresActions.loadAutores),
-    mergeMap(() => this.svc.getAll().pipe(
-      map(res => AutoresActions.loadAutoresSuccess({ autores: res.data ?? [] })),
-      catchError(err => of(AutoresActions.loadAutoresFailure({ error: err.message })))
-    ))
-  ));
+  loadAutores$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AutoresActions.loadAutores),
+      mergeMap(() =>
+        this.svc.getAll().pipe(
+          map((res) =>
+            AutoresActions.loadAutoresSuccess({ autores: res.data ?? [] }),
+          ),
+          catchError((err) =>
+            of(AutoresActions.loadAutoresFailure({ error: err.message })),
+          ),
+        ),
+      ),
+    ),
+  );
 
-  createAutor$ = createEffect(() => this.actions$.pipe(
-    ofType(AutoresActions.createAutor),
-    mergeMap(({ dto }) => this.svc.create(dto).pipe(
-      map(res => AutoresActions.createAutorSuccess({ autor: res.data! })),
-      catchError(err => of(AutoresActions.createAutorFailure({ error: err.message })))
-    ))
-  ));
+  createAutor$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AutoresActions.createAutor),
+      mergeMap(({ dto }) =>
+        this.svc.create(dto).pipe(
+          map((res) => AutoresActions.createAutorSuccess({ autor: res.data! })),
+          catchError((err) =>
+            of(AutoresActions.createAutorFailure({ error: err.message })),
+          ),
+        ),
+      ),
+    ),
+  );
 
-  updateAutor$ = createEffect(() => this.actions$.pipe(
-    ofType(AutoresActions.updateAutor),
-    mergeMap(({ id, dto }) => this.svc.update(id, dto).pipe(
-      map(res => AutoresActions.updateAutorSuccess({ autor: res.data! })),
-      catchError(err => of(AutoresActions.updateAutorFailure({ error: err.message })))
-    ))
-  ));
+  createAutorSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AutoresActions.createAutorSuccess),
+        tap(() => this.notification.success("Autor criado com sucesso!")),
+      ),
+    { dispatch: false },
+  );
 
-  deleteAutor$ = createEffect(() => this.actions$.pipe(
-    ofType(AutoresActions.deleteAutor),
-    mergeMap(({ id }) => this.svc.delete(id).pipe(
-      map(() => AutoresActions.deleteAutorSuccess({ id })),
-      catchError(err => of(AutoresActions.deleteAutorFailure({ error: err.message })))
-    ))
-  ));
+  updateAutor$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AutoresActions.updateAutor),
+      mergeMap(({ id, dto }) =>
+        this.svc.update(id, dto).pipe(
+          map((res) => AutoresActions.updateAutorSuccess({ autor: res.data! })),
+          catchError((err) =>
+            of(AutoresActions.updateAutorFailure({ error: err.message })),
+          ),
+        ),
+      ),
+    ),
+  );
 
-  constructor(private actions$: Actions, private svc: AutorService) {}
+  updateAutorSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AutoresActions.updateAutorSuccess),
+        tap(() => this.notification.success("Autor atualizado com sucesso!")),
+      ),
+    { dispatch: false },
+  );
+
+  deleteAutor$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AutoresActions.deleteAutor),
+      mergeMap(({ id }) =>
+        this.svc.delete(id).pipe(
+          map(() => AutoresActions.deleteAutorSuccess({ id })),
+          catchError((err) =>
+            of(AutoresActions.deleteAutorFailure({ error: err.message })),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  deleteAutorSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AutoresActions.deleteAutorSuccess),
+        tap(() => this.notification.success("Autor excluído com sucesso!")),
+      ),
+    { dispatch: false },
+  );
+
+  constructor(
+    private actions$: Actions,
+    private svc: AutorService,
+    private notification: NotificationService,
+  ) {}
 }
