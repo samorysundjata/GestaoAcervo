08f2526909ed93d02e9edcc06ebd95fcb882e196
Samory Sundjata <samory.sundjata@gmail.com>
2026-03-06 08:41:45 -0300

    Correção: ISBN no mapeamento do livro.

--- backend/Acervo/Acervo.Application/Mappings/LivroMappingConfig.cs
+++ backend/Acervo/Acervo.Application/Mappings/LivroMappingConfig.cs
@@ -8,9 +8,11 @@ public class LivroMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<Livro, LivroViewModel>();
+        config.NewConfig<Livro, LivroViewModel>()
+            .Map(dest => dest.ISBN, src => src.ISBN);
 
         config.NewConfig<Livro, LivroDetalheViewModel>()
+            .Map(dest => dest.ISBN, src => src.ISBN)
             .Map(dest => dest.AutorNome, src => src.Autor.Nome)
             .Map(dest => dest.GeneroNome, src => src.Genero.Nome);
     }
